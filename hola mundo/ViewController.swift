9f0f396ff15baf97be40727b22e0dd6f44886060
Pablo Sarmiento <pablo521@icloud.com>
2016-04-21 00:42:10 -0500

    commentary

--- hola mundo/ViewController.swift
+++ hola mundo/ViewController.swift
@@ -22,9 +22,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func changeMessage(sender: AnyObject) {
-        messageLabel.text = "hola \(nameTextField.text)"
-        view.endEditing(true)
+   
     }
 
 }
